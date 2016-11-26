bc4fc1cad93b8088f832a2d77103df67dbab443e
ForestSL <141220087@smail.nju.edu.cn>
2016-11-26 21:02:56 +0800

    Add files via upload
    数据库操作（登录注册查询点赞）

---
+++ database/src/DB_Login.java
@@ -0,0 +1,58 @@
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DB_Login extends DB_con{
+	
+	public String login(String name,String pwd){
+	
+		String result=null;
+		try {
+			connection(); 		//加载conn_db类，连接数据库；
+			boolean com = compareWithDB(name,pwd);
+			if(com==true)
+				result="登录成功";
+			else{
+				result="用户名或者密码不正确，请重新输入";
+			}
+		} 
+		catch (Exception e1) {
+			result="登录失败";
+			e1.printStackTrace();
+		}
+		return result;
+	}
+	
+	//判断数据是否在数据库中
+	boolean compareWithDB(String name,String password) throws Exception{	
+		Connection con = super.con;
+		Statement stmt = con.createStatement();
+		
+		String sql = "select * from userinfo";
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		while(rs.next()){				
+			//用户输入的信息和数据库中的信息做比较，判断输入是否正确；
+			String username = rs.getString(1);
+			String userpassword = rs.getString(2);
+			String state=rs.getString(3);
+			if(username.equals(name) && userpassword.equals(password))
+			{
+				state="2";//2表示在线
+				sql="update userinfo set userstate='"+state+"' where username='"+name+"'";
+				int judge=stmt.executeUpdate(sql);
+				if(judge>0)
+					System.out.println("状态修改成功");
+				else
+					System.out.println("状态修改失败");
+				return true;
+			}
+		}
+
+		return false;
+		
+	}
+
+}
---
+++ database/src/DB_Register.java
@@ -0,0 +1,69 @@
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DB_Register  extends DB_con{
+	//注册
+	public String register(String name,String pwd){	
+		String result=null;
+			
+		String state="1";//1表示离线
+			
+		try {
+			connection();
+			result=saveInDB(name,pwd,state);
+		} catch (SQLException e1) {
+			// TODO 自动生成的 catch 块
+			result="插入数据失败";
+			e1.printStackTrace();
+		}
+		return result;
+	}
+	
+	//注册信息写入数据库
+	public String saveInDB(String name,String password,String state) throws SQLException{
+		String result=null;
+		Connection con=super.con;
+		Statement stmt=con.createStatement();
+		
+		//创建表userinfo
+		String sql="create table if not exists userinfo(username varchar(10),userpassword varchar(20),userstate varchar(5))";
+		stmt.executeUpdate(sql);
+		//System.out.println("创建用户信息表成功");
+		result="创建用户信息表成功";
+		result+="\n";
+		
+		sql = "select * from userinfo";
+		ResultSet rs = stmt.executeQuery(sql);
+		boolean mjudge=false;//判断该用户是否已存在
+		while(rs.next()){				
+			//用户输入的信息和数据库中的信息做比较，判断输入是否正确；
+			String username = rs.getString(1);
+			if(username.equals(name))
+			{
+				result="该用户已存在";
+				mjudge=true;
+				break;
+			}
+		}
+		
+		if(mjudge==false)//用户不存在即注册
+		{
+			//插入信息
+			sql="insert into userinfo(username,userpassword,userstate) values('"+name+"','"+password+"','"+state+"')";
+			int judge=stmt.executeUpdate(sql);
+			if(judge>0)
+				result+="注册成功";
+				//System.out.println("注册成功");
+			else
+				result+="注册失败";
+				//System.out.println("注册失败");
+		}
+		
+		return result;
+		
+	}
+	
+}
